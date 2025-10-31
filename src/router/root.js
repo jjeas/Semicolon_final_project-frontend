449608f4b9056eb8c4f85856f3dcd9621cd696d1
전재석 <darkiss2355@gmail.com>
2025-10-31 11:48:13 +0900

    파일 구조 수정
    파일 구조 수정

--- src/router/root.js
+++ src/router/root.js
@@ -1,9 +1,12 @@
 import { lazy, Suspense } from "react";
 import Layout from "../components/layouts/Layout";
+import adminRouter from "./adminRouter";
 
 const { createBrowserRouter } = require("react-router-dom");
 
+const Loading = () => <div>Loading...</div>;
 const Main = lazy(() => import("../pages/public/Mainpage"));
+const Admin = lazy(() => import("../pages/admin/AdminPage"));
 
 const root = createBrowserRouter([
   {
@@ -17,6 +20,15 @@ const root = createBrowserRouter([
             <Main />
           </Suspense>
         ),
+      },
+      {
+        path: "admin",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Admin />
+          </Suspense>
+        ),
+        children: adminRouter(),
       },
     ],
   },
