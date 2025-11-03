e72f4d675d5ec368be36eda51c221a5306d68556
Shinyoung <dkssud3524@naver.com>
2025-11-03 12:24:17 +0900

    Page 추가
    Page 추가

---
+++ src/pages/member/MemberEditPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MemberEditPage = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                회원정보 수정
+            </li>
+            <li>
+                비밀번호 수정
+            </li>
+            <li>
+                
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default MemberEditPage
