fc0bef5146cb5354d797a67632893849ea0d0401
전재석 <darkiss2355@gmail.com>
2025-11-04 09:38:52 +0900

    헤더 UI 변경
    헤더 UI 변경

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const [hoveredMenuId, setHoveredMenuId] = useState(null);
 
   return (
-    <header className="bg-white shadow">
+    <header className="bg-white shadow border-b">
       {/* 상단 로그인/회원가입/마이페이지 */}
       <div className="bg-blue-950 flex space-x-8 justify-end px-8 py-3 text-sm text-white border-b border-gray-200">
         <Link to="/login" className="hover:underline mr-2">
@@ -24,9 +24,9 @@ const Header = () => {
       </div>
 
       {/* 메인 네비게이션 (Sub Menu 드롭다운을 위해 relative 설정) */}
-      
+
       <nav
-        className="relative flex justify-between items-center max-w-7xl mx-auto px-10 py-7 border-b border-gray-200"
+        className="relative flex justify-between items-center max-w-7xl mx-auto px-10 py-7 border-gray-200"
         onMouseLeave={() => setHoveredMenuId(null)} // NAV 영역 벗어나면 드롭다운 닫기
       >
         {/* 로고 */}
